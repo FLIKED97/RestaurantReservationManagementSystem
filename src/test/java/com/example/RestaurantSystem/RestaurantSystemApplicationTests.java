71b277e086df7e568378227fbff6371f16356161
Sasha <laplata228@gmail.com>
2024-08-03 14:47:22 +0300

    створив новий проєкт

---
+++ src/test/java/com/example/RestaurantSystem/RestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
